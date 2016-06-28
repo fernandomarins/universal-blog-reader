1e246632ee062f208b2992d28f743fb763883b28
Fernando Marins <f.augustomarins@gmail.com>
2016-06-28 17:23:39 -0300

    Added Pods and Brigdin-File

---
+++ Universal Blog/Universal-Blog-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Universal-Blog-Bridging-Header.h
+//  Universal Blog
+//
+//  Created by Fernando Augusto de Marins on 28/06/16.
+//  Copyright © 2016 Fernando, Inc. All rights reserved.
+//
+
+#import "SVProgressHUD.h"
+#import "MWFeedParser.h"
+#import <AFNetworking/UIImageView+AFNetworking.h>
+#import <KINWebBrowser/KINWebBrowserViewController.h>
